83472a32182399f956063c868d4c441da53b96b4
Mendy <emkay5771@outlook.com>
2023-06-30 09:28:11 -0700

    Subsections should work; added toggles and cleaned up UI/UX

--- pdfstream.py
+++ pdfstream.py
@@ -7,71 +7,32 @@ from datetime import date #type: ignore
 from datetime import datetime as dt #type: ignore
 from datetime import timedelta #type: ignore
 import streamlit as st #type: ignore
-import markdownlit
-from markdownlit import mdlit as mdlit
-import streamlit_toggle as stt
-from streamlit_pills_multiselect import pills
+import markdownlit #type: ignore
+from markdownlit import mdlit as mdlit #type: ignore
+import streamlit_toggle as stt #type: ignore
+from streamlit_pills_multiselect import pills #type: ignore
 import PyPDF2 #type: ignore
 from PyPDF2 import PdfMerger #type: ignore
 import glob
 
 st.set_page_config(page_title="PDF Section Selector", page_icon="📚", layout="wide", initial_sidebar_state="collapsed")
-
-def rambamenglish(dor, session, opt): # retrieves all rambam versions from chabad.org
-    pdf_options = {
-    'scale': scale2,
-    'margin-top': '0.1in',
-    'margin-right': '0.1in',
-    'margin-bottom': '0.1in',
-    'margin-left': '0.1in',
-    }
-    merger = PdfMerger()
-    if os.path.exists(f"Rambam{session}.pdf") != True:
-        for i in dor:
-            #st.write(dor)
-            #st.write("Rambam" + i)
-            driver = webdriver.Chrome(options=options)
-            lang = ""
-            chapters = ""
-            if "Rambam (3)-Bilingual" in opt:
-                    lang = "both"
-                    chapters = "3"
-            elif "Rambam (3)-Hebrew" in opt:
-                lang = "he"
-                chapters = "3"
-            elif "Rambam (3)-English" in opt:
-                lang = "primary"
-                chapters = "3"
-            elif "Rambam (1)-Bilingual" in opt:
-                lang = "both"
-                chapters = "1"
-            elif "Rambam (1)-Hebrew" in opt:
-                lang = "he"
-                chapters = "1"
-            elif "Rambam (1)-English" in opt:
-                lang = "primary"
-                chapters = "1"
-            driver.get(f"https://www.chabad.org/dailystudy/rambam.asp?rambamchapters={chapters}&tdate={i}#lt={lang}")
-            wait = WebDriverWait(driver, 10)
-            element = wait.until(EC.presence_of_element_located((By.ID, "content")))
-            pdf = driver.execute_cdp_cmd("Page.printToPDF", pdf_options)
-            with open(f"temp{session}.pdf", "ab") as f:
-                f.write(b64decode(pdf["data"]))
-            f.close()
-            driver.quit()
-
-            merger.append(f"temp{session}.pdf")
-
-        merger.write(f"Rambam{session}.pdf")
-        merger.close()
-        os.remove(f"temp{session}.pdf")
-
-
-
-def find_next_top_level_bookmark(toc, current_index):
+st.header("PDF Section Selector (BETA) 📚")
+def find_next_bookmark(toc, current_index):
     for i in range(current_index + 1, len(toc)):
+        print(f"I = {i}")
+        #TODO: Fix this so that it doesn't break when the last SUBsection is selected
+        if i == len(toc)-1:
+            print("Last section")
+            #if last section is selected, then return the last page of the pdf
+            return toc[i][2] - 1
         if toc[i][0] == 1:
-            return toc[i][2] - 2
+            return toc[i][2] - 1
+        if toc[i][0] == 2:
+            return toc[i][2] - 1
+        if toc[i][0] == 3:
+            return toc[i][2] - 1
+        if toc[i][0] == 4:
+            return toc[i][2] - 1
     return None
 
 def dedupe(pages, pages2, pages3, start_page, end_page): #dedupes the pages when appending, to ensure that pages aren't repeated
@@ -91,84 +52,63 @@ def dedupe(pages, pages2, pages3, start_page, end_page): #dedupes the pages when
         pages.append(end_page)
     return start_page,end_page
 
-def dynamicmake(contents, session): #compiles pdf after collecting all the necessary files
+def dynamicmake(session, contentsdict): #compiles pdf after collecting all the necessary files
     output_dir = ""
     toc = []
     doc_out = fitz.open()
     pages = []
     pages2 = []
     pages3 = []
-    print("dynamicmake")
+    #st.write(contentsdict)
+    print("dynamicmake2")
     toc = doc.get_toc() #get the table of contents
-    #st.write(optconv)
-    for q in contents:
-        for i, top_level in enumerate(toc): #type: ignore
-            #st.write(top_level)
-            try:
-                if not top_level[2]:
-                    continue  # skip top-level bookmarks without a page number
-                if top_level[1] == q:
-                    for j, sub_level in enumerate(toc[i+1:], start=i+1): #type: ignore
-                        if sub_level[0] != top_level[0] + 1:
-                            break  # stop when we reach the next top-level bookmark
-                        if z in sub_level[1]:
-                            start_page = sub_level[2] - 1
-                            if top_level[1] == q:
-                                end_page = toc[j+1][2] - 1 #type: ignore
-                                print("Chumash found")
-                            print(f"Current Start Page: {start_page}. Current End Page: {end_page}") #type: ignore
-                            start_page, end_page = dedupe(pages, pages2, pages3, start_page, end_page) #type: ignore
-                            print(f"New Start Page: {start_page}. New End Page: {end_page}")
-                            doc_out.insert_pdf(doc, from_page=start_page, to_page=end_page) #type: ignore
-                            continue
-            except:
-                continue
-            
-    #TODO: if selected option is sublevel, then find the next sublevel and append until then
-    for q in contents:
+    for q, n in contentsdict.items():
         for i, item in enumerate(toc): #type: ignore
             #st.write(item)
             #print(item)
             
-                if item[1] == q:
-                    print(item[1])
-                    #print("Likutei Sichos found")
-                    page_num_start = item[2] - 1
-                    print(page_num_start)
-                    page_num_end = find_next_top_level_bookmark(toc, i) #type: ignore
+            if item[1] == q:
+                print(f"Found: {item[1]}")
+                #print("Likutei Sichos found")
+                page_num_start = item[2] - 1
+                print(page_num_start)
+                if n == 1:
+                    page_num_end = find_next_bookmark(toc, i)
                     print(page_num_end)
-                    doc_out.insert_pdf(doc, from_page=page_num_start, to_page=page_num_end) #type: ignore
-                    break
+                elif n != 1:
+                    print("Getting next sublevel")
+                    page_num_end = find_next_bookmark(toc, i)
+                    print(page_num_end)
+                page_num_start, page_num_end = dedupe(pages, pages2, pages3, page_num_start, page_num_end) #type: ignore 
+                doc_out.insert_pdf(doc, from_page=page_num_start, to_page=page_num_end) #type: ignore
+                break
 
-                if q == 'Shnayim Mikra':
-                    print("Shnayim Mikra found")
-                    #st.write("Appending Shnayim Mikra")
-                    doc_out.insert_pdf(fitz.open(f"Shnayim{session}.pdf")) 
-                    print("Appended")
-                    continue
-                       
     doc_out.save(os.path.join(output_dir, f"output_dynamic{session}.pdf"))
     doc_out.close()
 
+
 def checkbox_callback(checkbox_key):
     st.session_state["checkbox"] = not st.session_state["checkbox"]
+
 if 'checkbox' not in st.session_state:
     st.session_state['checkbox'] = False
 uploaded_file = st.file_uploader("Upload a PDF file. NOTE: This file must have an outline for the program to work.", type=["pdf"]) #upload the pdf file
-#TODO: when bug is fixed, set default_value to False
-#TODO: make sublevel extraction work
-st.info("NOTE: Subsection listing might get very unwieldy. You have been warned.")
-sublevel_listing = stt.st_toggle_switch("Include Sublevels?", key="sublevel_listing", default_value=False) #toggle switch for sublevels
 
 if uploaded_file is None:
     st.subheader("Please upload a PDF file")
 if uploaded_file is not None:
+    doublesub = False
     st.subheader("Select the sections you would like to include in your PDF")
+    st.info("NOTE: Subsection listing might get unwieldy.")
+    sublevel_listing = stt.st_toggle_switch("Include Sublevels?", key="sublevel_listing", default_value=False) #toggle switch for sublevels
     doc = fitz.open(stream=uploaded_file.read(), filetype="pdf") #open the pdf file
     toc = doc.get_toc() #get the table of contents
     toclist = enumerate(toc)
     #st.write(toc)
     a=0
+    if sublevel_listing == True:
+        st.warning("NOTE: Turning on the below toggle may make the list super unwieldy. You have been warned.")
+        doublesub = stt.st_toggle_switch("Include Sublevels beyond one level?", key="sublevel_listing2", default_value=False) #toggle switch for sublevels
     for each in toclist:
         astr=str(a)
         if each[1][0] == 1:
@@ -183,27 +123,24 @@ if uploaded_file is not None:
                 st.session_state[checkbox_key] = False
             if sublevel_listing == True:
                 st.checkbox(f"SUBLEVEL: {each[1][1]}", key=checkbox_key, on_change=checkbox_callback, args=(checkbox_key,))
+                #st.write(each[1][1]+astr)
             a+=1
         elif each[1][0] == 3:
             checkbox_key = f"SUBLEVEL2: {each[1][1]+astr}"
             if checkbox_key not in st.session_state:
                 st.session_state[checkbox_key] = False
-            if sublevel_listing == True:
+            if doublesub == True:
                 st.checkbox(f"SUBLEVEL2: {each[1][1]}", key=checkbox_key, on_change=checkbox_callback, args=(checkbox_key,))
             a+=1
         elif each[1][0] == 4:
             checkbox_key = f"SUBLEVEL3: {each[1][1]+astr}"
             if checkbox_key not in st.session_state:
                 st.session_state[checkbox_key] = False
-            if sublevel_listing == True:
+            if doublesub == True:
                 st.checkbox(f"SUBLEVEL3: {each[1][1]}", key=checkbox_key, on_change=checkbox_callback, args=(checkbox_key,))
             a+=1
 
 with st.form(key="dvarform", clear_on_submit=False): #streamlit form for user input
-    st.header("PDF Section Selector (ALPHA) 📚")
-    st.warning("NOTE: This only seems to be working for full sections, but not partial subsections. Fix coming soon.")
-    st.info("If you select a subsection currently, it will include from the subsection selected through the entire section. Or it might not work at all 🤷‍♂️.")
-
     submit_button = st.form_submit_button(label="Generate PDF ▶️")
     
 
@@ -214,7 +151,7 @@ if submit_button: #if the user submits the form, run the following code, which w
     session = st.session_state['id']
     #st.write(session2)
     #st.write(session)
-    contents=[]
+    contentsdict={}
     toc = doc.get_toc() #get the table of contents #type: ignore
     toclist = enumerate(toc)
     #st.write(toc)
@@ -223,29 +160,37 @@ if submit_button: #if the user submits the form, run the following code, which w
         #st.write(key, value)
             
     for each in toclist:
+        astr=str(b)
         try:
-            astr=str(b)
             if st.session_state[each[1][1]+astr] == True:
-                print(each[1][1])
-                contents.append(each[1][1])
+                #print(each[1][1])
+                contentsdict[each[1][1]] = each[1][0]
                 #st.write(each[1][1])
-            elif st.session_state[f"SUBLEVEL: {each[1][1]+astr}"] == True:
-                print(each[1][1])
-                contents.append(each[1][1])
-                #st.write(each[1][1])
-            elif st.session_state[f"SUBLEVEL2: {each[1][1]+astr}"] == True:
-                print(each[1][1])
-                contents.append(each[1][1])
-                #st.write(each[1][1])
-            elif st.session_state[f"SUBLEVEL3: {each[1][1]+astr}"] == True:
-                print(each[1][1])
-                contents.append(each[1][1])
-                #st.write(each[1][1])
-        except:
-            pass
+        except KeyError:
+            try:
+                if st.session_state[f"SUBLEVEL: {each[1][1]+astr}"] == True:
+                    #print(each[1][1])
+                    contentsdict[each[1][1]] = each[1][0]
+                    #st.write(each[1][1])
+            except KeyError:
+                try:
+                    if st.session_state[f"SUBLEVEL2: {each[1][1]+astr}"] == True:
+                        #print(each[1][1])
+                        contentsdict[each[1][1]] = each[1][0]
+                        #st.write(each[1][1])
+                except KeyError:
+                    try:
+                        if st.session_state[f"SUBLEVEL3: {each[1][1]+astr}"] == True:
+                            #print(each[1][1])
+                            contentsdict[each[1][1]] = each[1][0]
+                            #st.write(each[1][1])
+                    except KeyError:
+                        print(f"KeyError: {each[1][1]+astr}")
+                        pass
         b+=1
     #st.write(contents)
-    dynamicmake(contents, session)
+    #st.write(contentsdict)
+    dynamicmake(session, contentsdict)
 
     if os.path.exists(f"output_dynamic{session}.pdf"):
         st.success("PDF created successfully!")
